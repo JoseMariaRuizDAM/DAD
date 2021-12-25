3c4581403a7f586da696dd0aa911d66240844fda
JoseMariaRuizDAM <91216717+JoseMariaRuizDAM@users.noreply.github.com>
2021-12-25 20:33:08 +0000

    comentarios añadidos ventana personal sanitario

--- ClinicaVista/ClinicaVista/PersonalSanitario.cs
+++ ClinicaVista/ClinicaVista/PersonalSanitario.cs
@@ -27,6 +27,11 @@ namespace ClinicaVista
 
         }
 
+        /**
+         * Función que funciona cuando se selecciona el botón salir
+         * Cuando se presiona, una ventana de dialogo que prgunta al usuario
+         * si quiere salir o no de la ventana 
+         */
         private void salirToolStripMenuItem_Click(object sender, EventArgs e)
         {
             DialogResult resultado = MessageBox.Show("¿Seguro que quieres salir?", "Salir", MessageBoxButtons.YesNo);
@@ -36,41 +41,49 @@ namespace ClinicaVista
             }
         }
 
+        //Muestra una ventana de dialogo cuando se presiona el boton Buscar Paciente
         private void buscarToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Buscar Paciente");
         }
 
+        //Muestra una ventana de dialogo cuando se presiona el botón "Ver ficha paciente"
         private void verFichaToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Ver ficha Paciente");
         }
 
+        //Muestra una ventana de dialogo cuando se presiona el botón "Sacar Etiqueta Paciente"
         private void sacarEtiquetaToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Sacar Etiqueta Paciente");
         }
 
+        //Muestra una ventana de dialogo cuando se presiona el botón "Actas Clinicas Paciente"
         private void actasClinicasToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Actas Clinicas Paciente");
         }
 
+        //Muestra una ventana de dialogo cuando se presiona el botón "Buscar Nota"
         private void buscarToolStripMenuItem1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Buscar Nota");
         }
 
+        //Muestra una ventana de dialogo cuando se presiona el botón "Crear Nota"
         private void crearToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Crear Nota");
         }
 
+        //Muestra una ventana de dialogo cuando se presiona el botón "Modificar Nota"
         private void modificarToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Modificar Nota");
         }
 
+        //Muestra una ventana de dialogo cuando se presiona el botón "Eliminar Nota"
         private void eliminarToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Eliminar Nota");
--- ClinicaVista/ClinicaVista/login.Designer.cs
+++ ClinicaVista/ClinicaVista/login.Designer.cs
@@ -29,70 +29,77 @@ namespace ClinicaVista
         /// </summary>
         private void InitializeComponent()
         {
-            this.botonLogin = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textUsuario = new System.Windows.Forms.TextBox();
-            this.textContraseña = new System.Windows.Forms.TextBox();
-            this.SuspendLayout();
-            // 
-            // botonLogin
-            // 
-            this.botonLogin.Location = new System.Drawing.Point(112, 133);
-            this.botonLogin.Name = "botonLogin";
-            this.botonLogin.Size = new System.Drawing.Size(100, 23);
-            this.botonLogin.TabIndex = 0;
-            this.botonLogin.Text = "Entrar";
-            this.botonLogin.UseVisualStyleBackColor = true;
-            this.botonLogin.Click += new System.EventHandler(this.botonLogin_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(51, 84);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Usuario";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(51, 114);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Contraseña";
-            // 
-            // textUsuario
-            // 
-            this.textUsuario.Location = new System.Drawing.Point(112, 77);
-            this.textUsuario.Name = "textUsuario";
-            this.textUsuario.Size = new System.Drawing.Size(100, 20);
-            this.textUsuario.TabIndex = 3;
-            // 
-            // textContraseña
-            // 
-            this.textContraseña.Location = new System.Drawing.Point(112, 107);
-            this.textContraseña.Name = "textContraseña";
-            this.textContraseña.Size = new System.Drawing.Size(100, 20);
-            this.textContraseña.TabIndex = 4;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(267, 202);
-            this.Controls.Add(this.textContraseña);
-            this.Controls.Add(this.textUsuario);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.botonLogin);
-            this.Name = "Form1";
-            this.Text = "Registro";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.botonLogin = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textUsuario = new System.Windows.Forms.TextBox();
+			this.textContraseña = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// botonLogin
+			// 
+			this.botonLogin.Location = new System.Drawing.Point(168, 205);
+			this.botonLogin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.botonLogin.Name = "botonLogin";
+			this.botonLogin.Size = new System.Drawing.Size(150, 35);
+			this.botonLogin.TabIndex = 0;
+			this.botonLogin.Text = "Entrar";
+			this.botonLogin.UseVisualStyleBackColor = true;
+			this.botonLogin.Click += new System.EventHandler(this.botonLogin_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(76, 129);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(64, 20);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Usuario";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(76, 175);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(92, 20);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Contraseña";
+			// 
+			// textUsuario
+			// 
+			this.textUsuario.Location = new System.Drawing.Point(168, 118);
+			this.textUsuario.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.textUsuario.Name = "textUsuario";
+			this.textUsuario.Size = new System.Drawing.Size(148, 26);
+			this.textUsuario.TabIndex = 3;
+			// 
+			// textContraseña
+			// 
+			this.textContraseña.Location = new System.Drawing.Point(168, 165);
+			this.textContraseña.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.textContraseña.Name = "textContraseña";
+			this.textContraseña.PasswordChar = '*';
+			this.textContraseña.Size = new System.Drawing.Size(148, 26);
+			this.textContraseña.TabIndex = 4;
+			// 
+			// login
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(400, 311);
+			this.Controls.Add(this.textContraseña);
+			this.Controls.Add(this.textUsuario);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.botonLogin);
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.Name = "login";
+			this.Text = "Registro";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
