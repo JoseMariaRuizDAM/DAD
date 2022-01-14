36d89ad4c320fc8e6cdc627b5051dd28e45d23f9
JoseMariaRuizDAM <91216717+JoseMariaRuizDAM@users.noreply.github.com>
2022-01-14 17:41:31 +0000

    calculadora

--- Calculadora_WPF/Calculadora_WPF/MainWindow.xaml.cs
+++ Calculadora_WPF/Calculadora_WPF/MainWindow.xaml.cs
@@ -23,6 +23,53 @@ namespace Calculadora_WPF
         public MainWindow()
         {
             InitializeComponent();
+            historial.Content = 0;
+            resultado.Text = "0";
+        }
+
+        private void Grid_Click(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement fe = e.Source as FrameworkElement;
+
+            switch (fe.Name)
+            {
+                case "num1":
+                    resultado.Text += "1";
+                    break;
+                case "num2":
+                    resultado.Text += "2";
+                    break;
+                case "num3":
+                    resultado.Text += "3";
+                    break;
+                case "num4":
+                    resultado.Text += "4";
+                    break;
+                case "num5":
+                    resultado.Text += "5";
+                    break;
+                case "num6":
+                    resultado.Text += "6";
+                    break;
+                case "num7":
+                    resultado.Text += "7";
+                    break;
+                case "num8":
+                    resultado.Text += "8";
+                    break;
+                case "num9":
+                    resultado.Text += "9";
+                    break;
+                case "sumar":
+                    resultado.Text += "+";
+                    break;
+                case "borrarTodo":
+                    resultado.Text = "0";
+                    break;
+                case "borraUno":
+                    resultado.Text = resultado.Text.Remove(resultado.Text.Length-1);
+                    break;
+            }
         }
     }
 }
